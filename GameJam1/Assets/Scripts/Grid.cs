d3f7150ae8b85cc61a169934611f6b6b07476e0d
MKiraly33 <matekiraly33@gmail.com>
2020-02-21 23:12:08 +0100

    Grid script added

---
+++ GameJam1/Assets/Scripts/Grid.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum structureType {NOTHING, WOOD, WOOD_TOWER, WOOD_GATHERER, WOOD_PROCESSER, PATH}
+
+public class Grid : MonoBehaviour
+{
+    public Vector2 pos;
+    public structureType structure;
+}
