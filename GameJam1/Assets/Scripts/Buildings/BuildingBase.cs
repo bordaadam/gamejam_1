efd23564b50fa9fbaf4d0dcfb348e14a86590f54
Adam Borda <borda.adam3@gmail.com>
2020-02-22 19:07:48 +0100

    Tower now shooting

---
+++ GameJam1/Assets/Scripts/Buildings/BuildingBase.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BuildingBase : MonoBehaviour
+{
+    [SerializeField] protected float shootingTime;
+    [SerializeField] protected float damage;
+    [SerializeField] protected GameObject target;
+    protected SphereCollider sphereCollider;
+    protected bool canShoot = true;
+
+    protected void Update()
+    {
+        if (canShoot && target != null)
+        {
+            Shoot();
+            StartCoroutine(Wait(shootingTime));
+        }
+    }
+
+    protected void OnTriggerStay(Collider col)
+    {
+        if (target == null)
+        {
+            target = col.gameObject;
+        }
+    }
+
+    protected void OnTriggerExit(Collider col)
+    {
+        target = null;
+    }
+
+    protected IEnumerator Wait(float time)
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(time);
+        canShoot = true;
+    }
+
+    protected private void Shoot()
+    {
+        if (target.GetComponent<Peasant>())
+        {
+            target.GetComponent<Peasant>().currentHealth -= damage;
+        }
+        if (target.GetComponent<Knight>())
+        {
+            target.GetComponent<Knight>().currentHealth -= damage;
+        }
+    }
+
+    protected void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if(target != null)
+            Gizmos.DrawLine(transform.position, target.transform.position);
+    }
+}
